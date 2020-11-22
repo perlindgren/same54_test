c9b882bbfe9cd798e73ae7e156a42ea63e9f4626
Per Lindgren <per.lindgren@ltu.se>
2020-11-22 15:28:27 +0100

    deadbeef

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,7 @@ fn main() {
     let mut session = Session::auto_attach("ATSAME54P20A").unwrap();
     let mut core = session.core(0).unwrap();
 
-    // Read a single 32 bit word.
-    let word = core.read_word_32(0x2000_0000).unwrap();
-
-    // Writing is just as simple.
+    // Write a block of 50 bit words.
     let buff = [0xdeadbeefu32; 50];
     core.write_32(0x2000_0000, &buff).unwrap();
 
@@ -19,18 +16,15 @@ fn main() {
 
     println!("word {:#X}", word);
 
-    // of course we can also write 8bit words.
+    // Write a block of 50 8bit words.
     let buff = [0x66; 50];
     core.write_8(0x2000_0000, &buff).unwrap();
-
-    // Read a single 32 bit word.
-    let word = core.read_word_32(0x2000_0000).unwrap();
 
     // Read a block of 50 32 bit words.
     let mut buff = [0u32; 50];
     core.read_32(0x2000_0000, &mut buff).unwrap();
 
-    for i in buff.into_iter() {
+    for i in buff.iter() {
         println!("buff {:#X}", i);
     }
 }
